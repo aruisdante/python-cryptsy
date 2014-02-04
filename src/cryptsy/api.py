05dcdd598edd20211edfb75f61ffc9a4b480af2e
Adam Panzica <adampanzica@gmail.com>
2014-02-03 23:59:39 -0500

    Added informational authenticated API calls, no JSON parsing

--- src/cryptsy/api.py
+++ src/cryptsy/api.py
@@ -82,3 +82,130 @@ def general_orderbook_data(market = None, timeout = None):
     else:
         data = call_pub_api('orderdata', [], timeout)
     return data
+
+def get_info(application_key, secret_key, timeout = None):
+    '''
+    Get's the user's account info
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('getinfo', [], application_key, secret_key, timeout)
+    return data
+
+def get_markets(application_key, secret_key, timeout = None):
+    '''
+    Get's the user's active markets
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('getmarkets', [], application_key, secret_key, timeout)
+    return data
+
+def get_transactions(application_key, secret_key, timeout = None):
+    '''
+    Get's the user's Deposit/Withdrawal history
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('mytransactions', [], application_key, secret_key, timeout)
+    return data
+
+def market_trades(market, application_key, secret_key, timeout = None):
+    '''
+    Get's the the last 1000 transactions for a market
+    :param market: The market ID to query
+    :type market: int
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('markettrades', [], application_key, secret_key, timeout)
+    return data
+
+def market_orders(market, application_key, secret_key, timeout = None):
+    '''
+    Get's the the set of buy/sell orders for a market
+    :param market: The market ID to query
+    :type market: int
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('marketorders', [('marketid',market)], application_key, secret_key, timeout)
+    return data 
+
+def my_trades(market = None, application_key, secret_key, limit = 200, timeout = None):
+    '''
+    Get's the the trade history for the user, optionally limited to a given market
+    :param market: (optional) The market ID to query
+    :type market: int
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param limit: The maximum number of transactions to list
+    :type limit: int
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = None
+    if market:
+        data = call_pri_api('marketorders', [('marketid',market), ('limit',market)], application_key, secret_key, timeout)
+    else:
+        data = call_pri_api('allmytrades', [], application_key, secret_key, timeout)
+    return data
+
+def my_orders(market = None, application_key, secret_key, timeout = None):
+    '''
+    Get's the the user's current open buy/sell orders, optionally limited to a a market
+    :param market: (optional) The market ID to query
+    :type market: int
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = None
+    if market:
+        data = call_pri_api('myorders', [('marketid',market)], application_key, secret_key, timeout)
+    else:
+        data = call_pri_api('allmyorders', [], application_key, secret_key, timeout)
+    return data 
+
+def depth(market, application_key, secret_key, timeout = None):
+    '''
+    Get's an array of buy and sell orders on the market representing market depth
+    :param market: The market ID to query
+    :type market: int
+    :param application_key: The application key to apply to the API call
+    :type application_key: str
+    :param secret_key: The user's secret key to apply to the API call
+    :type secret_key: str
+    :param timeout: Timeout for the request in seconds
+    
+    '''
+    data = call_pri_api('depth', [('marketid',market)], application_key, secret_key, timeout)
+    return data 
+
+
+
